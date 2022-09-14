13551888ef68e1c56c30b2fea722906a043814d1
abdmaliksajid <abdmaliksajid@gmail.com>
2022-09-14 20:35:18 +0700

    ini adalah pesan

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hallo Dunia!");
+
+//kita harus kasih tau dulu ke git file apa yang ingin kita track perubahannya
+
+//git add
+
+//kita ingin lihat apa saja yg berubah
+//git status
+
+//git commit -m ini adalah pesannya
